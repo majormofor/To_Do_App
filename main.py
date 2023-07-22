c902ef172dee5131e10ce63d8c1dc2d9790e6a9f
Uzochukwu Umejiofor <75849730+majormofor@users.noreply.github.com>
2023-07-22 12:31:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from webite import create_app
 app = create_app()
 
 if __name__ == '__main__': # needed before the app is ran
-    app.run(debug=True) # debug = true -- automatically restart server
+    app.run(debug=False, host = 0.0.0.0) # debug = true -- automatically restart server
